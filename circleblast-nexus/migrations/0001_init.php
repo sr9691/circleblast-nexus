3ff7d06a7b64bd4073178532cee498510f867518
ANSA Solutions <info@ansa.solutions>
2026-02-16 19:06:27 -0500

    removed dead folder and files from the project

--- circleblast-nexus/migrations/0001_init.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace CircleBlast\Nexus\Migrations;
-
-if (!defined('ABSPATH')) { exit; }
-
-function migration_0001_init(): void {
-  // Intentionally minimal for Iteration 1.
-  // Next iteration: create log table + core tables.
-}
